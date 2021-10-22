06681fd5e302474d96055aaa148d9168db9416d5
Tuoof <72484719+Tuoof@users.noreply.github.com>
2021-10-22 11:17:01 +0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SecurityCaseImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
